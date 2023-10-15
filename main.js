13e34ce828c46fdd8b06ce2b1b0d8203669434d5
EphRex <128737077+EphRex@users.noreply.github.com>
2023-10-15 11:04:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -29,13 +29,13 @@ document.onkeydown = function (e) {
     }
 }
 
-let counter = 0; let Title = 'D#6666'; let direction = true;
+let counter = 0; let Title = 'phRex'; let direction = true;
 aniTitle = setInterval(function () {
     if (counter == Title.length) direction = false;
     if (counter == false) direction = true;
     counter = (direction == true) ? ++counter : --counter;
     newtitle = (counter == 0) ? "" : Title.slice(0, counter);
-    document.title = 'F' + newtitle;
+    document.title = 'E' + newtitle;
 }, 400)
 
 window.oncontextmenu = function () {
